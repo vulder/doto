c84415dc7cd162678ca0b325db5aa39949b758c2
Florian Sattler <vuld3r@gmail.com>
2017-05-29 14:56:23 +0200

    added gentoo update script

---
+++ update_gentoo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+eix-sync
+
+emerge -auND --keep-going world
+
+read -r -p "Update 9999? [y/N] " response
+response=${response,,} # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]
+then
+  emerge -a1 neovim tig llvm-vim
+fi
+
+read -r -p "Build new compiler? [y/N] " response
+response=${response,,} # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]
+then
+  echo Building new Compiler
+  emerge -a1 --keep-going clang llvm compiler-rt compiler-rt-sanitizers llvm-libunwind clang-runtime libcxxabi libcxx libomp mesa
+
+  clang++ --version; clang_old++ --version
+
+  read -r -p "Install new compiler? [y/N] " response
+  response=${response,,} # tolower
+  if [[ "$response" =~ ^(yes|y)$ ]]
+  then
+    echo Updating clang
+    cp -r /usr/lib/llvm/5/* /usr/lib/llvm/5_old/
+  fi
+fi
+
