3fc982caaf6ee33d6f4de5b7bcceb24c412c4590
Florian Sattler <vuld3r@gmail.com>
2018-06-14 14:34:50 +0200

    update compiler packages for update script

--- update_gentoo.sh
+++ update_gentoo.sh
@@ -11,7 +11,7 @@ read -r -p "Update 9999? [y/N] " response
 response=${response,,} # tolower
 if [[ "$response" =~ ^(yes|y)$ ]]
 then
-  emerge -1 neovim tig llvm-common tmux
+  emerge -1 neovim tig tmux
 fi
 
 read -r -p "Build new compiler? [y/N] " response
@@ -19,7 +19,7 @@ response=${response,,} # tolower
 if [[ "$response" =~ ^(yes|y)$ ]]
 then
   echo Building new Compiler
-  emerge -1 --keep-going clang llvm compiler-rt compiler-rt-sanitizers llvm-libunwind clang-runtime libcxxabi libcxx libomp mesa lld lldb
+  emerge -1 --keep-going clang clang-common llvm llvm-common compiler-rt compiler-rt-sanitizers llvm-libunwind clang-runtime libcxxabi libcxx libomp mesa lld lldb
 
   clang++ --version; clang_old++ --version
 
