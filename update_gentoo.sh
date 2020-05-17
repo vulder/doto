3bcbc4421ec69f0b41855ffc68d4722d71705d45
Florian Sattler <vuld3r@gmail.com>
2020-05-17 22:29:22 +0200

    Updates gentoo update script

--- update_gentoo.sh
+++ update_gentoo.sh
@@ -11,7 +11,7 @@ read -r -p "Update 9999? [y/N] " response
 response=${response,,} # tolower
 if [[ "$response" =~ ^(yes|y)$ ]]
 then
-  emerge -1 neovim tig tmux
+  emerge -1 tig tmux
 fi
 
 read -r -p "Build new compiler? [y/N] " response
@@ -21,14 +21,7 @@ then
   echo Building new Compiler
   emerge -1 --keep-going clang clang-common llvm llvm-common compiler-rt compiler-rt-sanitizers llvm-libunwind clang-runtime libcxxabi libcxx libomp mesa lld lldb
 
-  clang++ --version; clang_old++ --version
-
-  #read -r -p "Install new compiler? [y/N] " response
-  #response=${response,,} # tolower
-  #if [[ "$response" =~ ^(yes|y)$ ]]
-  #then
-  #  echo Updating clang
-  #  cp -r /usr/lib/llvm/8/* /usr/lib/llvm/8_old/
-  #fi
+  clang++ --version
 fi
 
+emerge -a --depclean
