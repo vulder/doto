3962e8a58b9643b549b53ef1884591d4ab24abb1
Florian Sattler <vuld3r@gmail.com>
2021-10-04 21:42:30 +0200

    Ensure repo folder before running git submodule update

--- configure_system.sh
+++ configure_system.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+pushd() {
+  command pushd "$@" > /dev/null
+}
+popd() {
+  command popd "$@" > /dev/null
+}
+
 USER=$(whoami)
 REPO=$(pwd)
 
+pushd $REPO
 git submodule update --init --recursive
+popd
 
 ansible-playbook configure_system/main.yml --ask-become-pass \
   --extra-vars "{ 'user':$USER , 'repo':$REPO}"
