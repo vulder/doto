0cc21368337773ea942e4f13e0a6309bdce7d8e4
Florian Sattler <vuld3r@gmail.com>
2021-10-04 18:44:36 +0200

    Fix a few more issues and add todos

--- configure_system.sh
+++ configure_system.sh
@@ -3,5 +3,7 @@
 USER=$(whoami)
 REPO=$(pwd)
 
+git submodule update --init --recursive
+
 ansible-playbook configure_system/main.yml --ask-become-pass \
   --extra-vars "{ 'user':$USER , 'repo':$REPO}"
